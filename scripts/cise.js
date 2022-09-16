af49a45770354f5fbe571af124db94dd65cae4bc
Mark Schneider <mark.schneider@gmx.net>
2022-09-16 12:13:01 +0200

    Add dice .pngs

---
+++ scripts/cise.js
@@ -0,0 +1,23 @@
+Hooks.once('diceSoNiceReady', (dice3d) => {
+  dice3d.addSystem({id:"cise",name:"Cise"},true);
+  dice3d.addDicePreset({
+    type:"ds",
+    labels:[
+      'modules/fvtt-dsn-yoru/artwork/S1.png',
+      'modules/fvtt-dsn-yoru/artwork/S2.png',
+      'modules/fvtt-dsn-yoru/artwork/S3.png',
+      'modules/fvtt-dsn-yoru/artwork/S4.png',
+      'modules/fvtt-dsn-yoru/artwork/S5.png',
+      'modules/fvtt-dsn-yoru/artwork/S6.png'
+    ],
+    bumpMaps:[
+      'modules/fvtt-dsn-yoru/artwork/S1_bump.png',
+      'modules/fvtt-dsn-yoru/artwork/S2_bump.png',
+      'modules/fvtt-dsn-yoru/artwork/S3_bump.png',
+      'modules/fvtt-dsn-yoru/artwork/S4_bump.png',
+      'modules/fvtt-dsn-yoru/artwork/S5_bump.png',
+      'modules/fvtt-dsn-yoru/artwork/S6_bump.png'
+    ],
+    system:"cise"
+  });
+});
